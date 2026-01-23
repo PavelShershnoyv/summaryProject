ab8f02a3524d10cf026687cda66da25003e65c38
PavelShershnoyv <pavel.shershnoyv.02@mail.ru>
2026-01-23 19:32:19 +0500

    update cors

--- MSS/src/main/java/ru/development/MSS/config/SecurityConfig.java
+++ MSS/src/main/java/ru/development/MSS/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
         var config = new org.springframework.web.cors.CorsConfiguration();
-        config.setAllowedOrigins(java.util.List.of("http://localhost:3000"));
+        config.setAllowedOriginPatterns(java.util.List.of("*"));
         config.setAllowedMethods(java.util.List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(java.util.List.of("*"));
         config.setAllowCredentials(true);
